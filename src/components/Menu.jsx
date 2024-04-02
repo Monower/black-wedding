f75e8b62bf11abc3be2d0b96e6ab8a37a94b8022
monower <sadafahmed720@gmail.com>
2024-04-03 01:13:30 +0600

    minor change

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -12,27 +12,27 @@ const Menu = () => {
       <>
         <ul className="flex flex-col lg:flex-row lg:items-center  lg:gap-12 text-base lg:text-lg font-medium">
           <li className="border-b pb-1 lg:border-none lg:pb-0">
-            <a href="#" className={`text-brandBlack lg:${ loaded ? 'text-black' : 'text-white hover:underline' }`}>
+            <a href="#" className={`text-brandBlack ${ !loaded ? 'text-black' : 'text-white hover:underline' }`}>
               Vendors
             </a>
           </li>
           <li className="border-b pb-1 lg:border-none lg:pb-0">
-            <a href="#" className={`text-brandBlack lg:${ loaded ? 'text-black' : 'text-white hover:underline' }`}>
+            <a href="#" className={`text-brandBlack ${ !loaded ? 'text-black' : 'text-white hover:underline' }`}>
               Events
             </a>
           </li>
           <li className="border-b pb-1 lg:border-none lg:pb-0">
-            <a href="#" className={`text-brandBlack lg:${ loaded ? 'text-black' : 'text-white hover:underline' }`}>
+            <a href="#" className={`text-brandBlack ${ !loaded ? 'text-black' : 'text-white hover:underline' }`}>
               Magazine
             </a>
           </li>
           <li className="border-b pb-1 lg:border-none lg:pb-0">
-            <a href="#" className={`text-brandBlack lg:${ loaded ? 'text-black' : 'text-white hover:underline' }`}>
+            <a href="#" className={`text-brandBlack ${ !loaded ? 'text-black' : 'text-white hover:underline' }`}>
               Shop
             </a>
           </li>
           <li className="border-b pb-1 lg:border-none lg:pb-0">
-            <a href="#" className={`text-brandBlack lg:${ loaded ? 'text-black' : 'text-white hover:underline' }`}>
+            <a href="#" className={`text-brandBlack ${ !loaded ? 'text-black' : 'text-white hover:underline' }`}>
               Planning Tool
             </a>
           </li>
@@ -40,7 +40,7 @@ const Menu = () => {
             <li className="border-b pb-1 lg:border-none lg:pb-0">
               <a
                 href="#"
-                className={`text-brandBlack border lg:text-sm lg:${loaded ? 'text-black' : 'text-white lg:text-sm border-white' } lg:px-6 lg:py-3 rounded-full` }
+                className={`text-brandBlack border lg:text-sm ${!loaded ? 'text-black' : 'text-white lg:text-sm border-white' } lg:px-6 lg:py-3 rounded-full` }
               >
                 Login
               </a>
