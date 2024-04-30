55b346e36747df46e1550da737598e7b1f088b92
monower <sadafahmed720@gmail.com>
2024-04-30 22:44:59 +0600

    minor change

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -644,7 +644,23 @@ const Search = () => {
                 </form>
               </div>
             )}
-            <div>
+            <div className="flex flex-col lg:flex-row-reverse">
+              {viewType == "map" && (
+                <div>
+                  <iframe
+                    className="sticky top-24 z-10"
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d21890.219971094855!2d90.40693045845875!3d23.729817658863137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1714276935220!5m2!1sen!2sbd"
+                    width="500"
+                    height="300"
+                    style={{
+                      border: 0,
+                    }}
+                    allowFullScreen=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                  ></iframe>
+                </div>
+              )}
               <div>
                 <div
                   className={`flex flex-wrap gap-5 pb-10 ${
@@ -762,22 +778,6 @@ const Search = () => {
                   </button>
                 </div>
               </div>
-              {viewType == "map" && (
-                <div>
-                  <iframe
-                    className="sticky top-24 z-10"
-                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d21890.219971094855!2d90.40693045845875!3d23.729817658863137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1714276935220!5m2!1sen!2sbd"
-                    width="500"
-                    height="300"
-                    style={{
-                      border: 0,
-                    }}
-                    allowFullScreen=""
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                  ></iframe>
-                </div>
-              )}
             </div>
           </div>
         </div>
